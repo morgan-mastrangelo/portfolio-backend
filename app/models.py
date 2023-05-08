6f4b3d27dba1ff6ea3cb0ebd065a8ad52b7ddfbb
BLUECOMET <mmastrangelo1120@gmail.com>
2023-05-08 21:44:04 +0900

    ready to deploy

--- app/models.py
+++ app/models.py
@@ -4,4 +4,5 @@ class CommentModel(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     comment = models.TextField()
+    read = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
--- server/settings.py
+++ server/settings.py
@@ -42,7 +42,14 @@ INSTALLED_APPS = [
     'app'
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:4200",
+    "http://morgan-mastrangelo.github.io"
+]
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
